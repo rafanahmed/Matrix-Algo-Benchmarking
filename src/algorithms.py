d3858db88c7c931675c3fe2a08f34ef552d97f58
rafanahmed <rahmed7654@gmail.com>
2025-06-01 18:18:48 -0400

    Update algorithms.py

---
+++ src/algorithms.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def naive_algorithm(A, B):
+    N, M = A.shape
+    M_B, P = B.shape
+    if M != M_B:
+        raise ValueError(f"Incompatible matrix dimensions: Matrix A has {M} columns, but Matrix B has {M_B} rows.")
+    C = np.zeros((N, P))
+    for i in range(N):
+        for j in range(P):
+            for k in range(M):
+                C[i, j] += A[i, k] * B[k, j]
+    return C
